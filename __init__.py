c9fed059dfef6de5d5d37026f5907b4d4552dd52
Rhubarb Sin <rhubarbsin@gmail.com>
2013-01-25 15:19:07 -0500

    correct syntax

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ __version__ = '$Revision$'
 # $Source$
 
 from dnszone import ForwardZone, ReverseZone
-from dnsrecord import SOA, NS, A, AAAA, CNAME, MX, TXT PTR
+from dnsrecord import SOA, NS, A, AAAA, CNAME, MX, TXT, PTR
 from bindconf import BINDConf, ACL, View, Zone
