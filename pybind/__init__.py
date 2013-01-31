c327e1584fe5a9eae0d88b7f24a380e98ff52f6b
Rhubarb Sin <rhubarbsin@gmail.com>
2013-01-31 09:57:12 -0500

    reflect added class and newly non-public class

--- pybind/__init__.py
+++ pybind/__init__.py
@@ -2,4 +2,4 @@ __version__ = '0.1.0'
 
 from dnszone import ForwardZone, ReverseZone
 from dnsrecord import SOA, NS, A, AAAA, CNAME, MX, TXT, PTR
-from bindconf import BINDConf, ACL, View, Zone
+from bindconf import BINDConf, ACL, Masters, View, Zone
